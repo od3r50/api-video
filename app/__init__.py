892355a6a7191865275fca50084d6e12dafd2182
Anderson <andersonapp.osilva@gmail.com>
2025-04-08 19:04:42 -0300

    db installations

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from app.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 # from app.extensions import jwt
 # from app.auth.routes import auth_bp
 from app.video.routes import video_bp
 
+db = SQLAlchemy()
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    db.init_app(app)
+    Migrate(app, db)
 
     # jwt.init_app(app)
 
--- app/config.py
+++ app/config.py
@@ -8,3 +8,8 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret")
+    DB_USER = os.getenv("POSTGRES_USER", "postgres")
+    DB_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
+    DB_HOST = os.getenv("POSTGRES_HOST", "db")  # nome do serviço no docker-compose
+    DB_PORT = os.getenv("POSTGRES_PORT", "5432")
+    DB_NAME = os.getenv("POSTGRES_DB", "mydatabase")
