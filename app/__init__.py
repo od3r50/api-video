b2cdcf88a8dd173a10c8b285aa12206fdc9e761c
Anderson <andersonapp.osilva@gmail.com>
2025-04-08 21:21:39 +0000

    add postgres in .yaml & config and init adjusted

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
-# from app.config import Config
+from app.config import Config
 # from app.extensions import jwt
 # from app.auth.routes import auth_bp
 from app.video.routes import video_bp
 
 def create_app():
     app = Flask(__name__)
-    # app.config.from_object(Config)
+    app.config.from_object(Config)
 
     # jwt.init_app(app)
 
--- app/config.py
+++ app/config.py
@@ -3,3 +3,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 VIDEO_DIR = os.path.join(BASE_DIR, "..", "..", "videos")
 os.makedirs(VIDEO_DIR, exist_ok=True)
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret")
