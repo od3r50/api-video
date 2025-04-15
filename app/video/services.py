22ce7697dac61fbb0bee6a381e6038c6dfbe572b
Anderson <andersonapp.osilva@gmail.com>
2025-04-15 00:45:43 -0300

    job service created

--- app/video/services.py
+++ app/video/services.py
@@ -1,60 +1,15 @@
-from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip, CompositeAudioClip, AudioFileClip
-from app.services.file_utils import download_file
-from app.config import VIDEO_DIR
-import os
-from uuid import uuid4
-from threading import Thread
+from flask import jsonify
+from app.extensions import db
+from app.models.job import Job
 
-def render_clip_in_thread(clip, audio_clip, output, fps, job_id=None, jobs_dict=None):
-    def render():
-        try:
-            final_clip = clip.set_audio(audio_clip) if audio_clip else clip
-            final_clip.write_videofile(output, fps=fps)
+def get_job_by_id(id):
 
-            if jobs_dict is not None and job_id:
-                jobs_dict[job_id]["status"] = "done"
-                jobs_dict[job_id]["path"] = output
+    job = Job.query.get(id)
 
-        except Exception as e:
-            if jobs_dict is not None and job_id:
-                jobs_dict[job_id] = {"status": "error", "message": str(e)}
+    if not job:
+        return jsonify({"error": "Resource not found"}), 404
 
-    Thread(target=render).start()
+    return job
 
-def process_elements(data):
-    clips = []
-    audio_clips = []
-    temp_dirs = []
-
-    for el in data["elements"]:
-        tipo = el["type"]
-        time = el["time"]
-        duration = el["duration"]
-
-        if tipo == "video":
-            src = el["source"]
-            video_path, temp_dir = download_file(src)
-            temp_dirs.append(temp_dir)
-            clip = VideoFileClip(video_path).subclip(0, duration).set_start(time)
-            clips.append(clip)
-
-        elif tipo == "text":
-            txt = TextClip(
-                el["text"],
-                fontsize=el.get("font_size", 20),
-                color='white',
-                font="Arial-Bold"
-            ).set_position(("center", el["y"])).set_duration(duration).set_start(time)
-            clips.append(txt)
-
-        elif tipo == "audio":
-            src = el["source"]
-            audio_path, temp_dir = download_file(src)
-            temp_dirs.append(temp_dir)
-            audio = AudioFileClip(audio_path).subclip(0, duration).set_start(time)
-            audio_clips.append(audio)
-
-    final_clip = CompositeVideoClip(clips, size=(data["width"], data["height"]))
-    combined_audio = CompositeAudioClip(audio_clips) if audio_clips else None
-
-    return final_clip, combined_audio, temp_dirs
+def create_job(data):
+    return data
